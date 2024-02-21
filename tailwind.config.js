cd0aba05b1dedd6dbb56c17e9b82d4b8436f5db9
Morteza Gholamzade <mor.gholami.1@gmail.com>
2024-02-22 02:56:52 +0330

    project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      dropShadow: {
+        '3xl': '0 -2px 6px rgba(0, 0, 0, 0.16)',
+      },
+      width: {
+        '90': '353px',
+        '54': '216px',
+        '6xl': '690px',
+
+      },
+      height: {
+        '18': '76px',
+      },
+
+      boxShadow: {
+        '3xl': '0 -5px 15px rgba(0, 0, 0, 0.1)',
+      }
+    }
+    }, 
+  
+  plugins: [],
+}
+
