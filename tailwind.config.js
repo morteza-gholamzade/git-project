99593470ef9e5361a547541470c11af5e976efaf
Morteza Gholamzade <mor.gholami.1@gmail.com>
2024-02-26 01:03:32 +0330

    add desktop to page2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,14 @@ module.exports = {
         '3xl': '0 -2px 6px rgba(0, 0, 0, 0.16)',
       },
       width: {
+        '54': '218px',
+        '76': '300px',
         '90': '353px',
         '54': '216px',
+        '5xl': '463px',
         '6xl': '690px',
+        '7xl': '800px',
+        '8xl': '955px',
 
       },
       height: {
@@ -17,7 +22,11 @@ module.exports = {
       },
 
       boxShadow: {
-        '3xl': '0 -5px 15px rgba(0, 0, 0, 0.1)',
+        '3xl': '-10px -10px 15px rgba(0, 0, 0, 0.2)',
+        '4xl': '5px 5px 15px rgba(0, 0, 0, 0.2)',
+      },
+      translate: {
+        '1/5': '-50%',
       }
 
 
